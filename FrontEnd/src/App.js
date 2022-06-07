e2ada2417e5f49165c2d4820797e320f64739f2f
dollyllama <edolly2@protonmail.com>
2022-06-06 23:40:45 -0500

    "Loading Page"

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import LoadingPage from "./_pages/LoadingPage";
+
+function App() {
+  return (
+    <div className="App">
+      <LoadingPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ FrontEnd/src/_pages/LoadingPage.js
@@ -0,0 +1,27 @@
+import "./LoadingPage.css";
+
+import React from "react";
+
+const LoadingPage = () => {
+  return (
+    <div className="center">
+      <div className="wave-container">
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+      </div>
+      <div className="title-container">
+        <h1 className="title">EricDollinger.com</h1>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingPage;
