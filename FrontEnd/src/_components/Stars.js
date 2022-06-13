badd3005a51e148be2901f024bd4e9643b94dcf1
dollyllama <edolly2@protonmail.com>
2022-06-13 11:15:40 -0500

    "Stars"

---
+++ FrontEnd/src/_components/Stars.js
@@ -0,0 +1,17 @@
+import "./Stars.css";
+const Stars = () => {
+  return (
+    <div className="stars-container">
+      <section>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </section>
+    </div>
+  );
+};
+
+export default Stars;
