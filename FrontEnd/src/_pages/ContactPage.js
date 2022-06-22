d31fa851e1e9f5e7d64ff8685d082d08dab81a2a
dollyllama <edolly2@protonmail.com>
2022-06-22 00:25:59 -0500

    ""

--- FrontEnd/src/_pages/ContactPage.js
+++ FrontEnd/src/_pages/ContactPage.js
@@ -1,11 +1,28 @@
 import "./ContactPage.css";
 
-import React from "react";
+import { BsFacebook, BsGithub, BsTwitter } from "react-icons/bs";
 
+import { FaLinkedinIn } from "react-icons/fa";
 const ContactPage = () => {
   return (
-    <div className="page">
+    <div className="page contact-page">
       <h1 className="page-title">Contact Me</h1>
+      <ul className="top-nav-social-list">
+        <li className="social-link">
+          <FaLinkedinIn />
+        </li>
+        <li className="social-link">
+          <BsFacebook />
+        </li>
+        <li className="social-link">
+          <BsTwitter />
+        </li>
+        <li className="social-link">
+          <BsGithub />
+        </li>
+      </ul>
+
+      <h2 className="contact-form-title">Share Your Thoughts</h2>
       <div className="contact-form-container">
         <form>
           <div className="form-identity">
